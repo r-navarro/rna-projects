470d0e8a2b9be46debeece949bf3f1c39c7630a8
remi <rnavarro@objetdirect.com>
2015-03-18 08:34:25 +0100

    FullTank Management

--- src/main/resources/static/js/controllers/FullTankController.js
+++ src/main/resources/static/js/controllers/FullTankController.js
@@ -1,7 +1,11 @@
 angular.module('demo-hockey').controller('FullTankController',
 		function($scope, FullTankService, $routeParams) {
 
-			$scope.fullTank;
+			$scope.fullTank = {};
+			$scope.vehicleId = $routeParams.vehicleId;
 
+            $scope.create = function(fullTank){
+                FullTankService.create(fullTank);
+            }
 		}
 );
