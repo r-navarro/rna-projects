7bb11a14d0caa58cba3bab09eb403049f0c9b65c
remi <rnavarro@objetdirect.com>
2015-12-12 13:32:41 +0100

    add user management

---
+++ src/main/resources/static/components/user/UserService.js
@@ -0,0 +1,28 @@
+angular.module('carManagement.user')
+	.factory('UserService', ['DaoService',
+    function(DaoService) {
+
+    	get = function(){
+		    return DaoService.getData("users/", 'GET').then(function(response){
+		        return response.data;
+		    });
+		}
+
+		update = function(userId, user) {
+			return DaoService.getData("users/"+userId, 'PUT', user).then(function(response){
+		        return response.data;
+		    });
+		}
+
+		create = function(user) {
+			return DaoService.getData("users/", 'POST', user).then(function(response){
+		        return response.data;
+		    });
+		}
+
+    	return {
+    		get : get,
+    		update : update,
+    		create : create
+    	}
+}]);
