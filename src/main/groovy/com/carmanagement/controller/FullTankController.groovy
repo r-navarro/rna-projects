ecdabb87bd7363ccf865cff3156076efa01a4d4e
rnavarro <remi.navarro@dexstr.io>
2017-08-25 17:28:52 +0200

    add default sort for fuels

--- src/main/groovy/com/carmanagement/controller/FullTankController.groovy
+++ src/main/groovy/com/carmanagement/controller/FullTankController.groovy
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
+import org.springframework.data.web.SortDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.annotation.Secured
@@ -39,7 +41,7 @@ class FullTankController {
     @RequestMapping(value = "{vehicleId}/fullTanks", method = RequestMethod.GET)
     Page<FullTankDTO> findFullTankByVehiclePaginate(
             @PathVariable("vehicleId") String vehicleId,
-            @PageableDefault(size = FullTankController.PAGE_SIZE, page = 0) Pageable pageable) {
+            @SortDefault(sort = "date", direction = Sort.Direction.DESC) @PageableDefault(size = FullTankController.PAGE_SIZE, page = 0) Pageable pageable) {
 
         def fullTanksPage = fullTanksService.getFullTanks(pageable, vehicleId)
         def fullTanksDTO = fullTanksPage.content.collect { new FullTankDTO(it) }
